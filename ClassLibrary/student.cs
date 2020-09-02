c4ea588c1fc23d83b30dfd6afd75bfc0d75e012e
Damian Lentz <damianjlentz@outlook.com>
2020-09-02 00:04:51 -0500

    Student Set

---
+++ ClassLibrary/student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class student
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string firstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+
+        }
+
+        public string lastName
+        {
+            get { return _lastName;  }
+            set { _lastName = value; }
+        }
+    }
+}
