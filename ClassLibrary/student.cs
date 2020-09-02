c7065e0d3ea644e164f416ded72551c2df79fe70
Damian Lentz <damianjlentz@outlook.com>
2020-09-02 00:20:31 -0500

    Student Part 2

--- ClassLibrary/student.cs
+++ ClassLibrary/student.cs
@@ -6,22 +6,35 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    public class student
+    public class Student
     {
         private string _firstName;
         private string _lastName;
+        private string _id;
+        private float _gpa;
 
         public string firstName
         {
             get { return _firstName; }
             set { _firstName = value; }
-
         }
 
         public string lastName
         {
-            get { return _lastName;  }
+            get { return _lastName}
             set { _lastName = value; }
+        }
+
+        public string id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public float gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
         }
     }
 }
