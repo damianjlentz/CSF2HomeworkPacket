c67beeac536d4a33d5fbb0288cbcc05a5e533c99
Damian Lentz <damianjlentz@outlook.com>
2020-09-02 00:31:32 -0500

    Login

---
+++ ClassLibrary/Login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    class Login
+    {
+        private string _username;
+        private string _password;
+
+        public string username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+    }
+}
